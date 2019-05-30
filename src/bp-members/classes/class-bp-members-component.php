imath/BuddyPress

commit b5a081fbbb777218fc960095e2ddfc93367b052f

Take root profiles in account

src/bp-members/classes/class-bp-members-component.php
@@ -160,6 +160,7 @@ class BP_Members_Component extends BP_Component {
 			'slug'            => BP_MEMBERS_SLUG,
 			'root_slug'       => isset( $bp->pages->members->slug ) ? $bp->pages->members->slug : BP_MEMBERS_SLUG,
 			'has_directory'   => true,
+			'directory_slug'  => 'bp-members',
 			'directory_title' => isset( $bp->pages->members->title ) ? $bp->pages->members->title : $default_directory_title,
 			'search_string'   => __( 'Search Members...', 'buddypress' ),
 			'global_tables'   => array(
@@ -457,6 +458,14 @@ class BP_Members_Component extends BP_Component {
 		parent::setup_cache_groups();
 	}
 
+	/**
+	 * Add the component's rewrite tags.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param array $rewrite_tags Optional. See BP_Component::add_rewrite_tags() for
+	 *                            description.
+	 */
 	public function add_rewrite_tags( $rewrite_tags = array() ) {
 		// @todo set this in self::setup_globals();
 		$directory_rewrite_id                    = 'bp_members';
@@ -491,6 +500,14 @@ class BP_Members_Component extends BP_Component {
 		parent::add_rewrite_tags( $rewrite_tags );
 	}
 
+	/**
+	 * Add the component's rewrite rules.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param array $rewrite_rules Optional. See BP_Component::add_rewrite_rules() for
+	 *                             description.
+	 */
 	public function add_rewrite_rules( $rewrite_rules = array() ) {
 		// @todo use self::setup_globals().
 		$page_slug                               = 'page';
@@ -531,6 +548,18 @@ class BP_Members_Component extends BP_Component {
 		parent::add_rewrite_rules( $rewrite_rules );
 	}
 
+	/**
+	 * Add the component's directory permastructs.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param string $name   Optional. See BP_Component::add_permastructs() for
+	 *                       description.
+	 * @param string $struct Optional. See BP_Component::add_permastructs() for
+	 *                       description.
+	 * @param array  $args   Optional. See BP_Component::add_permastructs() for
+	 *                       description.
+	 */
 	public function add_permastructs( $name = '', $struct = '', $args = array() ) {
 		// @todo use self::setup_globals().
 		$directory_rewrite_id   = 'bp_members';
@@ -539,13 +568,42 @@ class BP_Members_Component extends BP_Component {
 		parent::add_permastructs( $directory_rewrite_id, $directory_slug . '/%' . $directory_rewrite_id . '%' );
 	}
 
+	/**
+	 * Parse the WP_Query and eventually display the component's directory or single item.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param WP_Query $query Required. See BP_Component::add_permastructs() for
+	 *                        description.
+	 */
 	public function parse_query( WP_Query $query ) {
 		if ( ! $query->is_main_query() || true === $query->get( 'suppress_filters' ) ) {
 			return;
 		}
 
 		// @todo use self::setup_globals().
-		$directory_rewrite_id   = 'bp_members';
+		$directory_rewrite_id = 'bp_members';
+		$directory_slug       = 'bp-members';
+
+		// Init the current member.
+		$member       = false;
+		$member_field = 'slug';
+		if ( bp_is_username_compatibility_mode() ) {
+			$member_field = 'login';
+		}
+
+		if ( bp_core_enable_root_profiles() ) {
+			global $wp;
+			$request_chunks = explode( '/', $wp->request );
+			$member_chunk   = reset( $request_chunks );
+
+			// Try to get an existing member.
+			$member = get_user_by( $member_field, $member_chunk );
+			if ( ! $member || ! bp_reset_query( 'members', $query ) ) {
+				return;
+			}
+		}
+
 		$is_members_component   = 1 === (int) $query->get( $directory_rewrite_id );
 		$bp                     = buddypress();
 
@@ -556,17 +614,14 @@ class BP_Members_Component extends BP_Component {
 			$member_slug            = $query->get( $single_item_rewrite_id );
 
 			if ( $member_slug ) {
-				$field = 'slug';
-
-				if ( bp_is_username_compatibility_mode() ) {
-					$field = 'login';
-				}
-
-				$member = get_user_by( $field, $member_slug );
-				if ( ! $member->ID ) {
-					$bp->current_component = '';
-					bp_do_404();
-					return;
+				// Unless root profiles are on, the member shouldn't be set yet.
+				if ( ! $member ) {
+					$member = get_user_by( $field, $member_slug );
+					if ( ! $member ) {
+						$bp->current_component = '';
+						bp_do_404();
+						return;
+					}
 				}
 
 				// Set the displayed user.

src/bp-core/bp-core-rewrites.php
@@ -1,0 +1,40 @@
+<?php
+/**
+ * BuddyPress Rewrites.
+ *
+ * @package BuddyPress
+ * @subpackage Core
+ * @since 6.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+function bp_reset_query( $component_id = '', WP_Query $query ) {
+	global $wp;
+	$bp = buddypress();
+
+	if ( ! bp_is_active( $component_id ) || ! isset( $bp->{$component_id}->directory_slug ) ) {
+		return false;
+	}
+
+	// Back up request uri.
+	$reset_server_request_uri = $_SERVER['REQUEST_URI'];
+
+	// Temporarly override it.
+	$_SERVER['REQUEST_URI'] = str_replace( $wp->request, $bp->{$component_id}->directory_slug . '/' . $wp->request, $reset_server_request_uri );
+
+	// Reparse request.
+	$wp->parse_request();
+
+	// Reparse query.
+	bp_remove_all_filters( 'parse_query' );
+	$query->parse_query( $wp->query_vars );
+	bp_restore_all_filters( 'parse_query' );
+
+	// Restore request uri.
+	$_SERVER['REQUEST_URI'] = $reset_server_request_uri;
+
+	// The query is reset.
+	return true;
+}

src/bp-core/classes/class-bp-component.php
@@ -236,6 +236,7 @@ class BP_Component {
 			'slug'                  => $this->id,
 			'root_slug'             => $default_root_slug,
 			'has_directory'         => false,
+			'directory_slug'        => '',
 			'directory_title'       => '',
 			'notification_callback' => '',
 			'search_string'         => '',
@@ -260,6 +261,15 @@ class BP_Component {
 		 * @param string $value Root directory slug.
 		 */
 		$this->root_slug             = apply_filters( 'bp_' . $this->id . '_root_slug',             $r['root_slug']             );
+
+		/**
+		 * Filters the component's top-level directory slug if available.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param bool $value Whether or not there is a top-level directory.
+		 */
+		$this->directory_slug        = apply_filters( 'bp_' . $this->id . '_directory_slug',        $r['directory_slug']        );
 
 		/**
 		 * Filters the component's top-level directory if available.

src/class-buddypress.php
@@ -474,6 +474,7 @@ class BuddyPress {
 		require( $this->plugin_dir . 'bp-core/bp-core-cssjs.php'            );
 		require( $this->plugin_dir . 'bp-core/bp-core-update.php'           );
 		require( $this->plugin_dir . 'bp-core/bp-core-options.php'          );
+		require( $this->plugin_dir . 'bp-core/bp-core-rewrites.php'         );
 		require( $this->plugin_dir . 'bp-core/bp-core-taxonomy.php'         );
 		require( $this->plugin_dir . 'bp-core/bp-core-filters.php'          );
 		require( $this->plugin_dir . 'bp-core/bp-core-attachments.php'      );

commit d77f6bd0af628916b66c0633a25f593dfec615e9

Fix a regression introduced by b5a081fbbb777218fc960095e2ddfc93367b052f

src/bp-members/classes/class-bp-members-component.php
@@ -628,7 +628,7 @@ class BP_Members_Component extends BP_Component {
 			if ( $member_slug ) {
 				// Unless root profiles are on, the member shouldn't be set yet.
 				if ( ! $member ) {
-					$member = get_user_by( $field, $member_slug );
+					$member = get_user_by( $member_field, $member_slug );
 					if ( ! $member ) {
 						$bp->current_component = '';
 						bp_do_404();