a0322c7ce177368b354594424d8e3883ef137b4a
imath <math.viet@gmail.com>
2019-09-26 01:16:31 +0200

    convert spaces in tabs

--- tests/phpunit/assets/bp-rest-api-controllers.php
+++ tests/phpunit/assets/bp-rest-api-controllers.php
@@ -18,9 +18,9 @@ class BP_REST_Attachments_Member_Avatar_Endpoint extends BP_REST_Mock_Class {
 	public function __construct() {
 		$this->namespace = bp_rest_namespace() . '/' . bp_rest_version();
 		$this->rest_base = 'members';
-    }
+	}
 
-    public function register_routes( $controller = '' ) {
+	public function register_routes( $controller = '' ) {
 		parent::register_routes( 'BP_REST_Attachments_Member_Avatar_Endpoint' );
 	}
 }
@@ -32,9 +32,9 @@ class BP_REST_Components_Endpoint extends BP_REST_Mock_Class {
 	public function __construct() {
 		$this->namespace = bp_rest_namespace() . '/' . bp_rest_version();
 		$this->rest_base = 'components';
-    }
+	}
 
-    public function register_routes( $controller = '' ) {
+	public function register_routes( $controller = '' ) {
 		parent::register_routes( 'BP_REST_Components_Endpoint' );
 	}
 }
@@ -46,9 +46,9 @@ class BP_REST_Members_Endpoint extends BP_REST_Mock_Class {
 	public function __construct() {
 		$this->namespace = bp_rest_namespace() . '/' . bp_rest_version();
 		$this->rest_base = 'members';
-    }
+	}
 
-    public function register_routes( $controller = '' ) {
+	public function register_routes( $controller = '' ) {
 		parent::register_routes( 'BP_REST_Members_Endpoint' );
 	}
 }
